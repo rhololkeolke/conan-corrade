afcebb8045fb7d669afb9e349bbabfe3ffc82cdb
Devin Schwab <digidevin@gmail.com>
2019-07-29 10:50:49 -0700

    :construction_worker: Using cpt in build.py

--- build.py
+++ build.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import copy
 
-
-from bincrafters import build_template_default
+from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
+    builder = ConanMultiPackager()
+    builder.add(
+        settings={"arch": "x86_64", "build_type": "Release"},
+        options={"dart:shared": False},
+    )
+    builder.add(
+        settings={"arch": "x86_64", "build_type": "Debug"},
+        options={"dart:shared": False},
+    )
 
-    builder = build_template_default.get_builder(pure_c=False)
+    # add c++17 build configs
+    new_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        new_settings = copy.copy(settings)
+        new_settings["compiler.cppstd"] = "17"
+        new_settings["compiler.libcxx"] = "libstdc++11"
+        new_builds.append([settings, options, env_vars, build_requires])
+        new_builds.append([new_settings, options, env_vars, build_requires])
+    builder.builds = new_builds
 
     builder.run()
